09fd10fd1e708a788d0d8886ce76c63fd8772a40
Korshikov Vladimir <vladimirkorz73@gmail.com>
2025-03-06 11:57:49 +0300

    real 3-pystushka

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')
+
 
 import readlineSync from 'readline-sync';
 
+console.log('Welcome to the Brain Games!');
+console.log('May I have your name?');
 const name = readlineSync.question('Your answer: ');
+console.log('Hello, ', name );
