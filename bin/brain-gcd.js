67040c7fe5639160138a4856f26a7ec706c994ad
vladimirkorz <vladimirkorz73@gmail.com>
2025-05-25 23:18:21 +0300

    brain-jcd02

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,4 +1,3 @@
-
 import readlineSync from 'readline-sync';
 
 const gcd = (a, b) => {
@@ -10,36 +9,29 @@ const gcd = (a, b) => {
     return a;
 };
 
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const playGame = () => {
+const brainGcd = () => {
     console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+    const userName = readlineSync.question('May I have your nickname? ');
+    console.log(`Hello, ${userName}!`);
     console.log('Find the greatest common divisor of given numbers.');
 
-    let correctAnswersCount = 0;
-
-    for (let i = 0; i < 3; i++) {
-        const num1 = getRandomInt(1, 100);
-        const num2 = getRandomInt(1, 100);
+    const rounds = 3; 
+    for (let i = 0; i < rounds; i++) {
+        const num1 = Math.floor(Math.random() * 100) + 1;
+        const num2 = Math.floor(Math.random() * 100) + 1;
         const correctAnswer = gcd(num1, num2);
 
         const userAnswer = readlineSync.question(`Question: ${num1} ${num2}\nYour answer: `);
 
         if (parseInt(userAnswer, 10) === correctAnswer) {
             console.log('Correct!');
-            correctAnswersCount += 1;
         } else {
             console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-            console.log(`Let's try again, ${name}!`);
+            console.log(`Let's try again, ${userName}!`);
             return;
         }
     }
-
-    console.log(`Congratulations, ${name}!`);
+    console.log(`Congratulations, ${userName}!`);
 };
-
-playGame();
 
 brainGcd();
