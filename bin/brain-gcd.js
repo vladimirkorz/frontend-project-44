ca7b66389e14a747b1686a9109edfc59a04e6fd0
vladimirkorz <vladimirkorz73@gmail.com>
2025-05-26 01:35:06 +0300

    yes, one more fix

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,37 +1,29 @@
 import readlineSync from 'readline-sync';
 
-const gcd = (a, b) => {
-    while (b !== 0) {
-        const temp = b;
-        b = a % b;
-        a = temp;
+const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+
+const playGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Find the greatest common divisor of given numbers.');
+
+  for (let i = 0; i < 3; i++) {
+    const num1 = Math.floor(Math.random() * 100) + 1;
+    const num2 = Math.floor(Math.random() * 100) + 1;
+    const correctAnswer = gcd(num1, num2);
+
+    const userAnswer = readlineSync.question(`Question: ${num1} ${num2}\nYour answer: `);
+
+    if (parseInt(userAnswer, 10) !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
-    return a;
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
 };
 
-const brainGcd = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your nickname? ');
-    console.log(`Hello, ${userName}!`);
-    console.log('Find the greatest common divisor of given numbers.');
-
-    const rounds = 3;
-    for (let i = 0; i < rounds; i++) {
-        const num1 = Math.floor(Math.random() * 100) + 1;
-        const num2 = Math.floor(Math.random() * 100) + 1;
-        const correctAnswer = gcd(num1, num2);
-
-        const userAnswer = readlineSync.question(`Question: ${num1} ${num2}\nYour answer: `);
-
-        if (parseInt(userAnswer, 10) === correctAnswer) {
-            console.log('Correct!');
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-            console.log(`Let's try again, ${userName}!`);
-            return;
-        }
-    }
-    console.log(`Congratulations, ${userName}!`);
-};
-
-brainGcd();
+playGame();
