ad0753c5d95a864138c883435efff584788c5f7f
vladimirkorz <vladimirkorz73@gmail.com>
2025-05-26 23:38:12 +0300

    fix-26.04

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,37 +1,26 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
-
 import { randomInt } from 'crypto';
-
-const getRandomNumber = () => randomInt(1, 100);
 
 const isEven = (num) => num % 2 === 0;
 
-const startGame = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${userName}!`);
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    let correctAnswers = 0;
+for (let i = 0; i < 3; i++) {
+  const question = randomInt(1, 100);
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
 
-    while (correctAnswers < 3) {
-        const question = getRandomNumber();
-        console.log(`Question: ${question}`);
-        const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-        const correctAnswer = isEven(question) ? 'yes' : 'no';
+  if (userAnswer !== correctAnswer) {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${userName}!`);
+    process.exit(1);
+  }
+  console.log('Correct!');
+}
 
-        if (userAnswer !== correctAnswer) {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-            console.log(`Let's try again, ${userName}!`);
-            return;
-        }
-
-        console.log('Correct!');
-        correctAnswers += 1;
-    }
-
-    console.log(`Congratulations, ${userName}!`);
-
-};
-
-startGame();
+console.log(`Congratulations, ${userName}!`);
