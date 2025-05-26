01c56b805d8fc475989be8df33a68331ce989044
vladimirkorz <vladimirkorz73@gmail.com>
2025-05-26 23:30:27 +0300

    Fix brain-games executable

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,15 +1,3 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-
-const greet = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
-
-// Запуск только при прямом вызове
-if (process.argv[1].includes('brain-games.js')) {
-  greet();
-}
-export default greet;
+console.log('Welcome to the Brain Games!');
+process.exit(0);
