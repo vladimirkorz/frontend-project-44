397ce4ac897bddc7c610979d2d561fe9bd3a2348
vladimirkorz <vladimirkorz73@gmail.com>
2025-05-26 23:34:20 +0300

    hate

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
-process.exit(0);
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
